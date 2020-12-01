90ee8ffcb1ff7b7d0364e996796401c8b8d4992a
Piyush Kumar <piyush.kumarmaloo@gmail.com>
2020-12-01 19:00:58 +0530

    Added details update

--- flaskblog/form.py
+++ flaskblog/form.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.model import User
+from flask_login import current_user
 
 
 class RegisterForm(FlaskForm):
@@ -27,3 +29,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), Length(min=2, max=20)])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateForm(FlaskForm):
+    username = StringField('User ID', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('E-Mail', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Pic', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username Taken. Please choose a new Username')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already Registered')
